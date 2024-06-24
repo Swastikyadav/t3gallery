bb8f5ee8b806e3d603f0861a89360cc4d6666284
Swastik Yadav <swastikb380@gmail.com>
2024-06-24 19:31:59 +0530

    add parallel routes

---
+++ src/app/@modal/default.tsx
@@ -0,0 +1,4 @@
+// Default allows next to render modal even when there is no content in there.
+export default function DefaultModal() {
+  return null;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Link from "next/link";
 import { SignedOut, SignedIn } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
 import Image from "next/image";
+import Link from "next/link";
 
 // Force renders the page on demand (on every refresh)
 export const dynamic = "force-dynamic";
@@ -28,7 +28,15 @@ async function Images() {
     <div className="flex flex-wrap justify-center gap-4">
       {images.map((image) => (
         <div key={image.id} className="flex w-48 flex-col">
-          <Image src={image.url} alt="upmages" width={480} height={480} />
+          <Link href={`/img/${image.id}`}>
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              alt={image.name}
+              width={480}
+              height={480}
+            />
+          </Link>
           <p>{image.name}</p>
         </div>
       ))}
