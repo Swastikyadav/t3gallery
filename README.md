807e240b41f7a4c2dbe812a8a87ade17c4aa6b2b
Swastik Yadav <swastikb380@gmail.com>
2024-06-24 21:07:32 +0530

    finalize modal and image parallel routes

--- README.md
+++ README.md
@@ -11,7 +11,7 @@
 - [x] Add image upload
 - [x] "taint" (server-only)
 - [x] Use Next/Image component
-- [ ] Routing/image page (parallel route)
+- [x] Routing/image page (parallel route)
 - [ ] Delete button (w/ server actions)
 - [ ] Analytics (posthog)
 - [ ] Ratelimiting (upstash)
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,16 +1,22 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import Image from "next/image";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
+  const userInfo = await clerkClient.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0 items-center justify-around">
       <div className="flex-shrink border-l">
         <Image src={image.url} alt={image.name} width={520} height={520} />
       </div>
 
-      <div className="flex w-48 flex-shrink-0 flex-col">
-        <div className="text-xl font-bold text-white">{image.name}</div>
+      <div className="flex w-1/2 flex-col text-white">
+        <div className="text-xl font-bold">{image.name}</div>
+        <div className="text-lg">Uploaded By: {userInfo.fullName}</div>
+        <div className="text-lg">
+          Created At: {image.createdAt.toLocaleDateString()}
+        </div>
       </div>
     </div>
   );
